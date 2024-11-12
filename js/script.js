46bda509e868268d4e17a2c99ac6affaf141ac32
sjaworowicz <125sjaworowicz@frhsd.com>
2024-11-12 09:33:42 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
 var list= $('li.hot') //.hide();
 console.log(list);
-$("li.hot").css("color", "red") .text(1);
+$("li.hot").css("color", "red") //.text(1);
 $("li.cold").css("color", "blue") .hide() .fadeIn(5000);
+
+console.log($('li:first-child').before("<li>Football</li>"));
